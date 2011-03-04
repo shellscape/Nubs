5209fa189fd0fac87f782dcc22fd45b0f00536b1
Andrew Powell <andrew@shellscape.org>
2011-03-04 17:02:07 -0500

    refactored

--- Hook/Nubs.Hook.cpp
+++ Hook/Nubs.Hook.cpp
@@ -18,81 +18,65 @@ UINT ShellMsg = 0;
 
 LRESULT CALLBACK ShellFunc (int nCode, WPARAM wParam, LPARAM lParam );
 
-BOOL APIENTRY DllMain( HANDLE /*hModule*/, 
-                       DWORD  ul_reason_for_call, 
-                       LPVOID /*lpReserved*/
-					 )
-{
-    switch (ul_reason_for_call)
-	{
+BOOL APIENTRY DllMain( HANDLE /*hModule*/, DWORD  ul_reason_for_call, LPVOID /*lpReserved*/) {
+    
+	switch (ul_reason_for_call) {
 		case DLL_PROCESS_ATTACH:
 		case DLL_THREAD_ATTACH:
 		case DLL_THREAD_DETACH:
 		case DLL_PROCESS_DETACH:
 			break;
-    }
-    return TRUE;
+	}
+
+	return TRUE;
 }
 
 
-int __stdcall CALLBACK InstallShellHook(int hwnd, int uintMsg)
-{
-	if (!hHookCBT) 
-	{
+int __stdcall CALLBACK InstallShellHook(int hwnd, int uintMsg) {
+	
+	if (!hHookCBT) {
 		hHookCBT = SetWindowsHookEx(WH_CBT, (HOOKPROC) ShellFunc, GetModuleHandle((LPCSTR)"NubHooks.dll"), 0);
-		if (!hHookCBT) 
+
+		if (!hHookCBT) {
 			return -1;
+		}
 
 		ShellWnd = (HWND)hwnd;
 		ShellMsg = (UINT)uintMsg;
 	}
-   else
-   {
-      MessageBox(0, "Error: Hook Not Set", "NubHooks", 0);
-   }
+	else {
+		MessageBox(0, "Error: Hook Not Set", "NubHooks", 0);
+	}
 
 	return 0;
 }
 
-BOOL __stdcall CALLBACK UninstallShellHook(HWND hwnd)
-{
+BOOL __stdcall CALLBACK UninstallShellHook(HWND hwnd) {
 	UnhookWindowsHookEx(hHookCBT);
 	hHookCBT = NULL;
 	return(TRUE);
 }
 
-LRESULT CALLBACK ShellFunc (int nCode, WPARAM wParam, LPARAM lParam )
-{
+LRESULT CALLBACK ShellFunc (int nCode, WPARAM wParam, LPARAM lParam) {
 	bool Handled = false;
 
-    if (nCode >= 0) 
-	{
-		switch (nCode)
-		{
-         case HCBT_ACTIVATE:
-         case HCBT_DESTROYWND:  
-         case HCBT_MOVESIZE:
-         {
-            PostMessage(ShellWnd, ShellMsg, nCode, wParam);
-         }
-         break;
-		//case HSHELL_WINDOWCREATED:
-		//case HSHELL_WINDOWDESTROYED:
-		//case HSHELL_ACTIVATESHELLWINDOW:
-		//case HSHELL_REDRAW:
-		//case HSHELL_WINDOWACTIVATED:
-		//	{
-  //          //MessageBox(0, "Activated", "Hook", 0);
-		//		PostMessage(ShellWnd, ShellMsg, nCode, wParam);
-		//		//Handled = true;
-		//	}
-		//	break;
+	if (nCode >= 0) {
+
+		switch (nCode) {
+			case HCBT_ACTIVATE:
+			case HCBT_DESTROYWND:  
+			case HCBT_MOVESIZE: {
+				PostMessage(ShellWnd, ShellMsg, nCode, wParam);
+			}
+			break;
 		}
 	};
 
-	if (!Handled)
+	if (!Handled) {
 		return CallNextHookEx(hHookCBT, nCode, wParam, lParam);
-	else
+	}
+	else {
 		return NULL;
+	}
 }
 
